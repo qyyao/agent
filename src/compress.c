139dd2eed290c1997aa4ebca771eb56d97b2867b
Annie Yao <annieyao@cedar1.cedar.computecanada.ca>
2023-07-16 16:46:58 -0700

    converted compress.c

--- src/compress.c
+++ src/compress.c
@@ -31,16 +31,16 @@ void buf_resize_comp(
 		uint8_t** dst, uint32_t* dst_sz,
 		uint8_t const* src, uint32_t src_sz) {
 	uint32_t comp_sz = FSE_compressBound(src_sz);
-	uint8_t* buf_bnd = resize_buf(*dst, comp_sz, 1);
+	uint8_t* buf_bnd = (uint8_t*)resize_buf(*dst, comp_sz, 1);
 	comp_sz = buf_compress(
 			buf_bnd, comp_sz,
 			src, src_sz);
 	uint8_t* buf_trim;
 	if (comp_sz == 0) { // Not compressible
-		buf_trim = resize_buf(buf_bnd, src_sz, 1);
+		buf_trim = (uint8_t*)resize_buf(buf_bnd, src_sz, 1);
 		memcpy(buf_trim, src, src_sz);
 	} else {
-		buf_trim = resize_buf(buf_bnd, comp_sz, 1);
+		buf_trim = (uint8_t*)resize_buf(buf_bnd, comp_sz, 1);
 	}
 	*dst = buf_trim;
 	*dst_sz = comp_sz;
